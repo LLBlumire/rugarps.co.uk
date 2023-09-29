d2f0b879ec7b6d3c436297ef76ca47a0124cba68
Lucille L. Blumire <lucy@llblumire.co.uk>
2023-09-29 14:44:54 +0100

    Add basic style and layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Atkinson_Hyperlegible, Inter } from "next/font/google";
+import Link from "next/link";
 
 export const atkinson = Atkinson_Hyperlegible({
   subsets: ["latin"],
@@ -29,7 +30,68 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${inter.variable} ${atkinson.variable}`}>
-      <body className="p-4">{children}</body>
+      <body className="flex flex-col min-h-[100dvh] items-center">
+        <header className="bg-[#E02217] w-full p-2 pb-4">
+          <Link href="/">
+            <h1 className="text-8xl text-center font-garps">GARPS</h1>
+          </Link>
+          <nav>
+            <ul className="flex flex-wrap gap-3 justify-center">
+              <li>
+                <Navlink to="/about" title="About" />
+              </li>
+              <li>
+                <Navlink to="/board-games" title="Board Games" />
+              </li>
+              <li>
+                <Navlink to="/ccgs" title="CCGs" />
+              </li>
+              <li>
+                <Navlink to="/wargames" title="Wargames" />
+              </li>
+              <li>
+                <Navlink to="/larp" title="LARP" />
+              </li>
+              <li>
+                <Navlink to="/ttrpgs" title="TTRPGs" />
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main className="flex-grow p-4 max-w-screen-lg w-full">{children}</main>
+        <footer className="bg-[#E02217] w-full p-2">
+          <nav>
+            <ul className="flex flex-wrap gap-3 justify-center">
+              <li>
+                <Navlink
+                  to="https://readingsu.co.uk/student-opportunities/view/garps"
+                  title="Student Union"
+                />
+              </li>
+              <li>
+                <Navlink
+                  to="https://www.instagram.com/rugarps"
+                  title="Instagram"
+                ></Navlink>
+              </li>
+              <li>
+                <Navlink
+                  to="https://discord.gg/xFZuwNCupG"
+                  title="Discord"
+                ></Navlink>
+              </li>
+            </ul>
+          </nav>
+        </footer>
+      </body>
     </html>
+  );
+}
+
+function Navlink({ to, title }: { to: string; title: string }) {
+  return (
+    <Link href={to} className="text-xl text-white font-bold hover:underline">
+      {title}
+    </Link>
   );
 }
---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+  return (
+    <>
+      <h2 className="text-2xl font-bold">Page Not Found</h2>
+      <p>
+        The page you were looking for might not exist yet, as this website is
+        still under construction.
+      </p>
+    </>
+  );
+}
